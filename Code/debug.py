963335c7cc8c26986065e0277a10f73f9c6c143a
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-23 21:09:15 -0500

    It works (mostly)
    I just need to figure out how to regenerate stats if the figures are too low.

--- Code/debug.py
+++ Code/debug.py
@@ -22,3 +22,28 @@ def assign_stats():
     for stat in jimothy.stats:
         jimothy.stats[stat] = generate_stat()
         statSpread.append(jimothy.stats[stat])
+
+def check_stat_total(spread = statSpread):
+    newStatTotal = 0
+    for check in spread:
+        newStatTotal += check
+    return newStatTotal
+
+def check_number_bigness(array = statSpread):
+    bigNumbers = 0
+    for number in array:
+        if number >= 15:
+            bigNumbers += 1
+    return bigNumbers
+
+def pass_or_fail(statTotal, bigNumbers):
+    if statTotal >= 75 and bigNumbers >= 2:
+        return True
+    else:
+        return False
+    
+assign_stats()
+check_stat_total()
+
+print(statSpread, check_stat_total(), check_number_bigness())
+print(pass_or_fail(check_stat_total(), check_number_bigness()))
