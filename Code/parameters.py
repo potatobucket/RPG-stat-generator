412ec0140c47ec15e96b5b5c6644433bdecdb755
potatobucket <104957727+potatobucket@users.noreply.github.com>
2023-07-25 05:56:34 -0500

    Changed CSV parser to DictReader
    Endless tweaking is the hallmark of a good creative.

--- Code/parameters.py
+++ Code/parameters.py
@@ -8,14 +8,14 @@ lastNames = []
 suffixes = []
 
 with open("Roleplaying Names List.csv", "r") as names:
-    csv_reader = csv.reader(names)
-    next(csv_reader)
+    csv_reader = csv.DictReader(names)
+    # next(csv_reader)
     for line in csv_reader:
-        if line[0] != "":
-            prefixes.append(line[0])
-        if line[1] != "":
-            firstNames.append(line[1])
-        if line[2] != "":
-            lastNames.append(line[2])
-        if line[3] != "":
-            suffixes.append(line[3])
+        if line["Prefix"] != "":
+            prefixes.append(line["Prefix"])
+        if line["First Name"] != "":
+            firstNames.append(line["First Name"])
+        if line["Last Name"] != "":
+            lastNames.append(line["Last Name"])
+        if line["Suffix"] != "":
+            suffixes.append(line["Suffix"])
